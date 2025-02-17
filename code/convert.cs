f858cea7f1fe8f248a9dc2b3e92f136e5dca1a9f
Stankoski-Sacha <sacha.stankoski.001@student.uni.lu>
2025-02-17 05:07:25 +0100

    Money and Token System added

---
+++ code/convert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Casino.code
+{
+    class convert
+    {
+        public static void Convert()
+        {
+            int money = data.Balance;
+            int tokens = data.Tokens;
+
+            Console.WriteLine("Convert to 1) Tokens or 2) Money?");
+            String action = Console.ReadLine().ToLower().Trim();
+
+            switch (action)
+            {
+                case "1":
+                    Console.WriteLine("How many ? 1 Token = 10 money");
+                    int amount = int.Parse(Console.ReadLine());
+                    if (money < amount)
+                    {
+                        Console.WriteLine("You do not have enough money.");
+                    }
+                    else
+                    {
+                        data.Balance -= amount;
+                        data.Tokens += amount / 10;
+                    }
+                    break;
+
+            }
+        }
+    }
+}
--- code/menu.cs
+++ code/menu.cs
@@ -16,11 +16,12 @@ namespace C__Casino.code
                 Console.WriteLine("     Welcome to the C# Casino!   ");
                 Console.WriteLine("=================================");
                 Console.WriteLine("1. Blackjack");
-                Console.WriteLine("5. Balance");
-                Console.WriteLine("6. Exit"); 
+                Console.WriteLine("6. Convert Tokens/Money");
+                Console.WriteLine("7. Balance");
+                Console.WriteLine("8. Exit"); 
                 String action = Console.ReadLine().ToLower().Trim();
 
-                if (action == "6") break;
+                if (action == "8") break;
 
                 switch (action)
                 {
@@ -28,8 +29,12 @@ namespace C__Casino.code
                         Blackjack blackjack = new Blackjack();
                         blackjack.main();
                         break;
-                    case "5":
+                    case "6":
+                        convert.Convert();
+                        break;
+                    case "7":
                         Console.WriteLine("Your balance is: " + data.Balance);
+                        Console.WriteLine("Your tokens are: " + data.Tokens);
                         break;
                     default:
                         Console.WriteLine("Invalid action. Please try again.");
@@ -39,3 +44,4 @@ namespace C__Casino.code
         }
     }
 }
+    
